eff89f01da505b76c0f7c8603fd03d17ab92f47d
vikrambhagat16 <102277591+vikrambhagat16@users.noreply.github.com>
2022-03-24 18:18:06 +0530

    Newly Added Code
    Assignment_1_HelloWorld

---
+++ Assignment_1_HelloWorld/Assignment_1_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment_1_HelloWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var transaction = new Transaction(50, DateTime.Now, "Coffee");
+            transaction.printAccountDetails();
+            transaction = new Transaction(232, DateTime.Now, "Tea");
+            transaction.printAccountDetails();
+            transaction = new Transaction(123, DateTime.Now, "Tea Bag");
+            transaction.printAccountDetails();
+
+        }
+    }
+}
---
+++ Assignment_1_HelloWorld/Assignment_1_HelloWorld/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_HelloWorld
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+        public Transaction(decimal amount, DateTime date, string notes)
+        {
+            Amount = amount;
+            Date = date;
+            Notes = notes;
+        }
+
+        public void printAccountDetails()
+        {
+            Console.WriteLine("Amount : "+Amount + " Date : "+ Convert.ToDateTime(Date).ToString("dd/MM/yyyy"));
+        }
+    }
+
+}
